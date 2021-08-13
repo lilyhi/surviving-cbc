db38c0d6af3b4effcf1f5b6483450d849d81e042
LifeInvader <matthewkausfox@hotmail.com>
2021-08-13 14:11:17 -0600

    started on initializing the models. Added Post and Event code, will need to be looked over

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,1 +1,31 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
+const postSchema = new Schema(
+    {
+        postText: {
+            type: String,
+            required: 'You need to leave a post!',
+            minlength: 1,
+            maxlength: 680
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+    },
+    // {
+    //     toJSON: {
+    //         getters: true
+    //     }
+    // }
+);
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
