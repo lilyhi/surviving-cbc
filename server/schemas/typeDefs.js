545906dd09c4636d22215360f5a9c466efbf752d
LifeInvader <matthewkausfox@hotmail.com>
2021-08-13 14:43:39 -0600

    Added starting code for TypeDefs. Will need revisions

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,51 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    posts: [Post]
+    event: [Event]
+  }
+
+  type Post {
+    _id: ID
+    postText: String
+    createdAt: String
+    username: String
+  }
+
+  type Event {
+    _id: ID
+    eventText: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    posts(username: String): [Post]
+    post(_id: ID!): Post
+
+    events(username: String): [Event]
+    event(_id: ID!): Event
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addPost(postText: String!): Post
+    addEvent(eventText: String!): Event 
+  }
+`;
+
+module.exports = typeDefs;
