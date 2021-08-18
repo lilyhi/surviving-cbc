dce435d54eb8c673be4250645dd0a1bee7a48e78
pablo de la cruz <pablodlc@gmail.com>
2021-08-18 01:55:19 -0700

    implementing login/signup functinoality

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 // import all components into the landing page here.
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { InMemoryCache, createHttpLink, useQuery, gql } from "@apollo/client";
+import ApolloClient from 'apollo-boost';
+// import { ApolloProvider } from '@apollo/react-hooks';
 
 import Header from './components/Header';
 import Home from './pages/Home';
 import Suli from "./pages/Suli";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // going to add Switch later
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from "@apollo/client";
-// import { ApolloProvider } from '@apollo/react-hooks';
+import NoMatch from './pages/NoMatch';
+import SinglePost from './pages/SinglePostView';
+import SingleEvent from './pages/SingleEventView';
+import Footer from './components/Footer';
 
 
 // import CreatePostButton from './components/CreatePostButton';
@@ -14,49 +19,56 @@ import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from "@apo
 
 // import Event from './components/Event';
 // import CreateEventButton from './components/CreateEventButton'
-import Footer from './components/Footer';
+
+const client = new ApolloClient({
+    request: operation => {
+        const token = localStorage.getItem('id_token');
+
+        operation.setContext({
+            headers: {
+                authorization: token ? `Bearer ${token}` : ''
+            }
+        });
+    },
+    uri: '/graphql'
+});
 
 function App() {
 
-  const client = new ApolloClient({
-    uri: 'https://48p1r2roz4.sse.codesandbox.io',
-    cache: new InMemoryCache()
-  });
+    return (
+        <ApolloProvider client={client} >
+            <Router>
+                <Header />
 
-  return (
-    <ApolloProvider client={client} >
-      <Router>
-        <Header />
+                <Switch>
+                    <Route exact path='/'>
+                        <Home />
+                    </Route>
 
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
+                    <Route path='/home' exact>
+                        <Home />
+                    </Route>
 
-          <Route path='/home' exact>
-            <Home />
-          </Route>
+                    <Route path='/signup' exact>
+                        <Suli />
+                    </Route>
 
-          <Route path='/signup' exact>
-            <Suli />
-          </Route>
+                    <Route path='/login' exact>
+                        <Suli />
+                    </Route>
 
-          <Route path='/login' exact>
-            <Suli />
-          </Route>
+                    <Route path='/register' exact>
+                        <Suli />
+                    </Route>
 
-          <Route path='/register' exact>
-            <Suli />
-          </Route>
+                    <Route path='/iwillsurvive' exact>
+                        <Suli />
+                    </Route>
 
-          <Route path='/iwillsurvive' exact>
-            <Suli />
-          </Route>
-
-        </Switch>
-        <Footer />
-      </Router>
-    </ApolloProvider>
-  );
+                </Switch>
+                <Footer />
+            </Router>
+        </ApolloProvider>
+    );
 }
 export default App;
---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatch = () => {
+    return (
+        <div>
+            If you're a survivor, you've seen a few errors in your day. Here's another. No match found.
+        </div>
+    );
+};
+
+export default NoMatch;
