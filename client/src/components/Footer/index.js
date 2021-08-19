c5f235992e4dc745860675ce6088ab76b2990055
pablo de la cruz <pablodlc@gmail.com>
2021-08-19 03:44:42 -0700

    adding slightly styled footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,52 +1,64 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import GitHubIcon from '@material-ui/icons/GitHub';
+import { FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
 
     return (
 
-        <div className="footer mt-5">
+        <div className="footer mt-5" style={{ width: '100%', backgroundColor: '#F75F1C', padding: "0 48px" }}>
             <footer className="">
                 <Row>
-                    <h2 className="">Suggestions, comments, concerns, and complaints on Surviving-CBC are welcome! Reach out to us at the links below.</h2>
+                    <h3 className="" style={{ textAlign: 'center', fontFamily: "'Lato', sans-serif", padding: "24px", fontWeight: 'bold' }}>Surviving CBC wants your feedback! Suggestions, comments, concerns, and questions can be sent to the creators of Surviving CBC using the links below.</h3>
                 </Row>
-
-                <Row>
-                    <h2 className="">Surviving-CBC was made with ❤️️ by the following survivors:</h2>
-                </Row>
+                <h4 className="" style={{ textAlign: 'center', fontFamily: "'Lato', sans-serif", padding: '12px', fontWeight: 'bold' }}>Made with ❤️️ by the original four SCBC Survivors!</h4>
 
                 <Row>
                     <Col>
-                        <h2>Lily</h2>
-                        <a href="https://github.com/lilyhi" target="blank">
-                            <GitHubIcon fontSize='large' style={{ textDecoration: 'none', color: '#5d5c61', margin: '24px' }} />
-                        </a>
-                    </Col>
-                    <Col>
-                        <h2>Luke</h2>
-                        <a href="https://github.com/Shilohjones194" target="blank">
-                            <GitHubIcon fontSize='large' style={{ textDecoration: 'none', color: '#5d5c61', margin: '24px' }} />
-                        </a>
-                    </Col>
-                    <Col>
-                        <h2>Matt</h2>
-                        <a href="https://github.com/LifeInvaderr" target="blank">
-                            <GitHubIcon fontSize='large' style={{ textDecoration: 'none', color: '#5d5c61', margin: '24px' }} />
-                        </a>
-                    </Col>
-                    <Col>
-                        <h2>Pablo</h2>
-                        <a href="https://github.com/pablodlc" target="blank">
-                            <GitHubIcon fontSize='large' style={{ textDecoration: 'none', color: '#5d5c61', margin: '24px' }} />
-                        </a>
-                    </Col>
+                        <div>
+                            <a href='https://github.com/lilyhi' target='blank'>
+                                <h1 style={{ textAlign: 'center', textDecoration: 'none', color: 'black' }}><FaGithub /></h1>
+                                <h4 style={{ textAlign: 'center', fontFamily: "'Lato', sans-serif", color: 'black', fontWeight: 'bold' }}>Lily</h4>
+                            </a>
 
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                            <a href='https://github.com/https://github.com/Shilohjones194' target='blank'>
+                                <h1 style={{ textAlign: 'center', color: 'black' }}><FaGithub /></h1>
+                                <h4 style={{ textAlign: 'center', fontFamily: "'Lato', sans-serif", color: 'black', fontWeight: 'bold' }}>Luke</h4>
+                            </a>
 
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                            <a href='https://github.com/https://github.com/LifeInvaderr' target='blank'>
+                                <h1 style={{ textAlign: 'center', color: 'black' }}><FaGithub /></h1>
+                                <h4 style={{ textAlign: 'center', fontFamily: "'Lato', sans-serif", color: 'black', fontWeight: 'bold' }}>Matt</h4>
+                            </a>
+
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                            <a href='https://github.com/pablodlc' target='blank'>
+                                <h1 style={{ textAlign: 'center', color: 'black' }}><FaGithub /></h1>
+                                <h4 style={{ textAlign: 'center', color: 'black', fontFamily: "'Lato', sans-serif", fontWeight: 'bold' }}>Pablo</h4>
+                            </a>
+
+                        </div>
+                    </Col>
                 </Row>
+
+                {/* <a href="https://github.com/pablodlc" target="blank">
+                        <GitHubIcon className= fontSize='large' style={{ textDecoration: 'none', color: '#5d5c61', margin: '24px' }} />
+                    </a> */}
+
             </footer>
-        </div>
+        </div >
 
 
     )
